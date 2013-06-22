b4f53cdbac699a280c9f90c23bf77e9cd0cd1355
Hidabe <fhidalgo@sopinet.com>
2013-06-22 12:12:57 +0200

    First commit structural

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ colorizebundle
 ==============
 
 Bundle for Symfony2 from Colorize library
+
+This library is in AlphaState
+
+TO-DOing
+=======
+- Add css filter (as less, yui, etc)
+- Check external libraries injection from Colorize Library
---
+++ SopinetColorizeBundle.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* This file is part of SopinetColorizeBundle.
+*
+* (c) 2013 by Fernando Hidalgo - Sopinet
+*/
+
+namespace Sopinet\ColorizeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SopinetColorizeBundle extends Bundle
+{
+}
