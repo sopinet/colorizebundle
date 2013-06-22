ac661d994c251783943972a9511eb7c198ef692e
Hidabe <fhidalgo@sopinet.com>
2013-06-22 17:24:39 +0200

    added debug function

--- Assetic/Filter/ColorizeFilter.php
+++ Assetic/Filter/ColorizeFilter.php
@@ -4,6 +4,7 @@ namespace Sopinet\ColorizeBundle\Assetic\Filter;
  
 use Assetic\Asset\AssetInterface;
 use Assetic\Filter\FilterInterface;
+use Sopinet\Colorize\ColorizeService;
  
 class ColorizeFilter implements FilterInterface
 {
@@ -16,6 +17,7 @@ class ColorizeFilter implements FilterInterface
     {
         $content = $asset->getContent();
         //Do something to $content
-        $asset->setContent($content);
+        $img_src = "http://www.sopinet.com/layout/bootstrap/template/sopinetoliva_mini.png";
+        $asset->setContent(autoColorizeFromString($content, $img_src));
     }
 }
